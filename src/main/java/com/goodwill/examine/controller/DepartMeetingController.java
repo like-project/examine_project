43382747a875a8048f6bb55e9c1c0e5b77a69a26
李珂 <ke.li@naxions.com>
2025-02-24 13:57:38 +0800

    3334444

--- src/main/java/com/goodwill/examine/controller/DepartMeetingController.java
+++ src/main/java/com/goodwill/examine/controller/DepartMeetingController.java
@@ -42,5 +42,12 @@ public class DepartMeetingController {
   }
 
 
+  @PutMapping(value = "/update")
+  public Result<Object> updateDepartMeeting(@RequestBody DepartMeetingDTO departMeetingDTO) {
+    // 调用服务层执行部门会议添加操作
+    departMeetingService.addDepartMeeting(departMeetingDTO);
+    return Result.success();
+  }
+
 
 }
